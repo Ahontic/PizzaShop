e953918218a8ab9fe259b444fb31bcfadc3f95fe
Quix <nswells14@gmail.com>
2019-02-13 15:33:20 +0200

    Display products step1

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 function something()
 {
-	var x = window.localStorage.getItem('bbb');
-	x = x * 1 +1;
+	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+	x = x * 1 + 1; // умножением преобразовываем string в int
 
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 	
 
 	alert (x);
