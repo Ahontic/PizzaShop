4cd065d0246ff35b3e82185078ee73ca9d572e2c
Quix <nswells14@gmail.com>
2019-02-16 16:50:37 +0200

    Made input hidden, updated input on page reload

--- public/app.js
+++ public/app.js
@@ -19,7 +19,14 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 	// вывод количества продуктов в корзине
-	alert('items in your cart: ' + cart_get_number_of_items());
+	
+	update_orders_input();
+}
+
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
 }
 
 function cart_get_number_of_items()
